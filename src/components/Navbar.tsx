6db4886bc22338df53eabc28c87b3b2fac6a4fe1
Amit Mittal <mittalmailbox@gmail.com>
2021-04-13 17:42:05 +0530

    Removed blank object

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,9 @@ import { Flex } from "@chakra-ui/layout";
 import React, { ReactElement } from "react";
 import { Link } from "react-router-dom";
 
-interface Props {}
 
-export default function Navbar({}: Props): ReactElement {
+
+export default function Navbar(): ReactElement {
   return (
     <Flex>
       <Link to="/" style={{width:"100%"}}><Flex p="2" justifyContent="center" width="full">
