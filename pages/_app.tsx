fa1f7cee0d007ceb05bdee2f46ca79f42a4258c3
Amit Mittal <mittalmailbox@gmail.com>
2021-07-07 21:14:39 +0530

    Do not show languages popup automatically

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import App from "next/app";
 import "../src/App.css";
-import { useState } from "react";
+import { useEffect, useState, lazy ,Suspense} from "react";
 import Head from "next/head";
 import { ColorModeSwitcher } from "./../src/ColorModeSwitcher";
-import { ColorModeScript } from "@chakra-ui/react";
+import {
+  ColorModeScript,
+  createStandaloneToast,
+  Text,
+  VStack,
+  Heading,
+} from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { fetchData, getLanguages } from "./../src/utils";
 import { Hydrate } from "react-query/hydration";
-import Router from "next/router"; 
+import Router from "next/router";
 import NProgress from "nprogress"; //nprogress module
 import "nprogress/nprogress.css"; //styles of nprogress
 import theme from "./../src/theme";
@@ -15,14 +21,16 @@ import {
   ChakraProvider,
   Box,
   Grid,
-  Spinner,
+  Button,
   Flex,
   IconButton,
 } from "@chakra-ui/react";
 import Navbar from "./../src/components/Navbar";
-import Languages from "./../src/components/Languages";
+
 import { SettingsIcon } from "@chakra-ui/icons";
+import { FiX } from "react-icons/fi";
 import useLanguages from "../hooks/useLanguages";
+import dynamic from 'next/dynamic'
 
 const queryClient = new QueryClient();
 let preSelectedLanguages: string = "";
@@ -32,16 +40,76 @@ if (typeof window !== "undefined") {
     localStorage.setItem("selected-languages", JSON.stringify(["English"]));
   }
 }
+const toast = createStandaloneToast();
+
+const Languages=  dynamic(() => import("./../src/components/Languages"));
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
+function showToast(setShowLanguagesModal){
+  toast({
+    title: "Languages",
+    description: "",
+    status: "info",
+    duration: 90000,
+    isClosable: true,
+    render() {
+      return (
+        <Flex
+          position="relative"
+          color="black"
+          p={3}
+          pt="6"
+          gridGap="20px"
+          bg="yellow.100"
+          borderRadius="xl"
+          alignItems="center"
+        >
+          <VStack alignItems="flex-start">
+            <Heading size="sm">Update Languages</Heading>
+            <Text>Showing English Channels, change language?</Text>
+          </VStack>
+
+          <Button
+            colorScheme="blackAlpha"
+            onClick={() => {
+              toast.closeAll();
+              setShowLanguagesModal(true);
+            }}
+          >
+            Yes
+          </Button>
+          <IconButton
+            onClick={() => toast.closeAll()}
+            _hover={{
+              backgroundColor: "rgba(0,0,0,0.2)",
+            }}
+            background="transparent"
+            width="20px"
+            height="20px"
+            position="absolute"
+            top="5px"
+            right="5px"
+            aria-label=""
+            icon={<FiX />}
+          ></IconButton>
+        </Flex>
+      );
+    },
+  });
+
+}
 export default function MyApp({ Component, pageProps, data }) {
+  
   const [selectedLanguages, setSelectedLanguages] = useLanguages();
-  const [showLanguagesModal, setShowLanguagesModal] = useState<boolean>(
-    !preSelectedLanguages
-  );
+  const [showLanguagesModal, setShowLanguagesModal] = useState<boolean>(false);
+  useEffect(() => {
+    if (!preSelectedLanguages) {
+      setTimeout(() => showToast(setShowLanguagesModal), 4000)
+    }
+  }, []);
   const toggleLanguage = (language: string) => {
     let newLanguages = [];
     if (!selectedLanguages.includes(language)) {
@@ -61,9 +129,9 @@ export default function MyApp({ Component, pageProps, data }) {
       <ChakraProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
           <Hydrate state={pageProps.dehydratedState}>
-          
             <Box fontSize="xl">
               <Navbar />
+          
               <Languages
                 selectedLanguages={selectedLanguages}
                 toggleSelect={toggleLanguage}
@@ -71,6 +139,7 @@ export default function MyApp({ Component, pageProps, data }) {
                 isOpen={showLanguagesModal}
                 onClose={() => setShowLanguagesModal(false)}
               />
+               
               <Grid p={3}>
                 <Flex justifyContent="flex-end">
                   <IconButton
